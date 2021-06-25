f55f76502a7be50e08003e11a471fe3d10397cfd
Luuk de Gram <Luukdegram@users.noreply.github.com>
2021-06-25 20:49:59 +0200

    Improve request parser and add test cases for it

--- src/Request.zig
+++ src/Request.zig
@@ -7,7 +7,7 @@ const std = @import("std");
 const uri = @import("uri.zig");
 
 /// Parsed URL from the client's request.
-url: uri.Uri,
+uri: uri.Uri,
 
 /// Parses a request, validates it and then returns a `Request` instance
 pub fn parse(reader: anytype, buffer: []u8) Parser(@TypeOf(reader)).Error!Request {
@@ -17,12 +17,18 @@ pub fn parse(reader: anytype, buffer: []u8) Parser(@TypeOf(reader)).Error!Reques
 
 /// All error cases which can be hit when validating
 /// a Gemini request.
-pub const ParserError = error{
-    /// Request is invalid, the first line does not end with \r\n
+pub const ParseError = error{
+    /// Request is invalid, the first line does not end with \r\n.
     MissingCRLF,
-    /// The URL has a maximum length of 1024 bytes, including its scheme.
-    URLTooLong,
-};
+    /// Request is missing mandatory URI.
+    MissingUri,
+    /// The URI has a maximum length of 1024 bytes, including its scheme.
+    UriTooLong,
+    /// When the provided buffer is smaller than 1024 bytes.
+    BufferTooSmall,
+    /// Connection was closed by the client.
+    EndOfStream,
+} || uri.ParseError;
 
 /// Constructs a generic `Parser` for a given `ReaderType`.
 fn Parser(comptime ReaderType: type) type {
@@ -32,7 +38,7 @@ fn Parser(comptime ReaderType: type) type {
         buffer: []u8,
         index: usize,
 
-        const Error = ReaderType.Error || ParserError;
+        const Error = ReaderType.Error || ParseError;
 
         /// Initializes a new `Parser` with a reader of type `ReaderType`
         fn init(reader: ReaderType, buffer: []u8) Self {
@@ -41,22 +47,51 @@ fn Parser(comptime ReaderType: type) type {
 
         /// Parses the request and validates it
         fn parseAndValidate(self: *Self) Error!Request {
-            const request_line = try self.reader.readUntilDelimiterOrEoF(self.buffer, '\n');
-            if (request_line.len < 3) return error.MissingCRLF;
+            if (self.buffer.len < 1024) return error.BufferTooSmall;
+            const read = try self.reader.read(self.buffer);
+            if (read == 0) return error.EndOfStream;
+            if (read == 2) return error.MissingUri;
+
+            // URI's have a maximum length of 1024 bytes including its scheme
+            if (read > 1026) {
+                return error.UriTooLong;
+            }
 
             // verify CRLF
-            if (!std.mem.eql(u8, request_line[request_line.len - 2 ..], "\r\n")) {
+            if (!std.mem.eql(u8, self.buffer[read - 2 ..], "\r\n")) {
                 return error.MissingCRLF;
             }
 
-            const url_len = request_line.len - 2;
-
-            // URL's have a maximum length of 1024 bytes including its scheme
-            if (request_line.len > 1026) {
-                return error.URLTooLong;
-            }
-
-            return Request{ .url = try url.parse(self.buffer[0..url_len]) };
+            return Request{ .uri = try uri.parse(self.buffer[0 .. read - 2]) };
         }
     };
+}
+
+test "Happy flow" {
+    const valid = "gemini://example.com/hello-world\r\n";
+    var buf: [1024]u8 = undefined;
+    _ = try parse(std.io.fixedBufferStream(valid).reader(), &buf);
+}
+
+test "Unhappy flow" {
+    const error_cases = .{
+        .{ "gemini://example.com", error.MissingCRLF },
+        .{ &[_]u8{0} ** 1027, error.UriTooLong },
+        .{ "\r\n", error.MissingUri },
+        .{ "", error.EndOfStream },
+    };
+
+    inline for (error_cases) |case| {
+        var buf: [1028]u8 = undefined;
+        try std.testing.expectError(
+            case[1],
+            parse(std.io.fixedBufferStream(case[0]).reader(), &buf),
+        );
+    }
+
+    var buf: [1]u8 = undefined;
+    try std.testing.expectError(
+        error.BufferTooSmall,
+        parse(std.io.fixedBufferStream("test").reader(), &buf),
+    );
 }
--- src/lemon_pie.zig
+++ src/lemon_pie.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
+const Request = @import("Request.zig");
 
 test {
     _ = @import("uri.zig");
+    _ = Request;
 }
